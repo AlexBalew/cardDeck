9e29e94cfd4e6a92ea5a32c16532de40a08b4226
AlexBalew <balew48@gmail.com>
2021-11-07 18:35:44 +0300

    css&testPage(test comps are added)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css';
 import {HashRouter} from "react-router-dom";
 import Header from './components/header/header';
 import CardDeckRoutes from './components/routes/cardDeckRoutes';
+import './App.css'
 
 
 function App() {
   return (
-    <div>
+    <div className='appStyle'>
         <HashRouter>
             <Header/>
             <CardDeckRoutes/>
