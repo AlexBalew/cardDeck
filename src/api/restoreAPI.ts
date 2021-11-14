0f59eaa916fe76d8bcf423f3b362da8463d84619
AlexBalew <balew48@gmail.com>
2021-11-14 15:14:41 +0300

    Info message(after successful request) is added to the project

--- src/api/restoreAPI.ts
+++ src/api/restoreAPI.ts
@@ -13,7 +13,7 @@ export const restoreAPI = {
     forgot(value: string) {
         return instance.post<{ info: string, error: string }>('/auth/forgot', {
             email: value,
-            from: "test-front-admin <balew48@gmail.com>",
+            from: `test-front-admin <>`,
             message: `<div style="background-color: lime; padding: 15px">password recovery link: <a href='http://localhost:3000/#/set-new-password/$token$'>link</a></div>`
         }, {})
     }
