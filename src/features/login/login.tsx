360206b506cd67b761699c8233f78552a77ca2fb
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2021-11-15 14:06:58 +0300

    m3 - added new styles for inputs, button and text in the login.tsx

--- src/features/login/login.tsx
+++ src/features/login/login.tsx
@@ -1,37 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import {NavLink} from "react-router-dom";
 import s from "./login.module.css"
+import {PATH} from "../../app/Routes";
+import viewPassword from "../../assets/viewPassword.png"
+import hiddenPassword from "../../assets/hiddenPassword.png"
+import SuperButton from "../../common/elements/button/SuperButton";
 
-type LoginPropsType = {}
+
+type LoginPropsType = {
+
+
+}
+
 
 export const Login = (props: LoginPropsType) => {
+
+    const [show, setShow] = useState(false)
+
+    const handleShowPassword = () => {
+        setShow(!show)
+    }
     return (
         <div className={s.loginContainer}>
             <div className={s.formBlock}>
                 <div className={s.content}>
                     <h3 className={s.fromBlockTitle}>It-incubator</h3>
+
                     <form className={s.formContent}>
                         <label>Email</label>
-                            <div className={s.formEmailInput}>
-                                <input type="email" autoComplete="email"/>
-                            </div>
+                        <div className={s.formInputBox}>
+                            <input type="email" className={s.formInput} autoComplete="email"/>
+                        </div>
 
                         <label>Password</label>
-                            <div className={s.formPasswordInput}>
-                                <input type="password" />
-                            </div>
+                        <div className={s.formInputBox}>
+                            <input type="password" className={s.formInput}/>
+                            <img className={s.inputPasswordView} src={show ? viewPassword : hiddenPassword} alt="eye" onClick={handleShowPassword}/>
+                        </div>
 
                         <label><input type="checkbox"/>Remember</label>
 
                         <div className={s.formButtonBlock}>
-                            <button className={s.btn}>LOGIN</button>
-
-                            <p className={s.formBlockText}>Forgot password?
-                                <a className={s.formLink} href="#">Click Here</a></p>
-                            <p className={s.formBlockText}>Don't have an account
-                                <a className={s.formLink} href="#">Sign up</a></p>
+                            <SuperButton className={s.btn}>LOGIN</SuperButton>
                         </div>
                     </form>
+
+                    <div className={s.textBlock}>
+                        <div>
+                            <span className={s.formBlockText}>Forgot password?</span>
+                            <NavLink to={PATH.RESTORE_PASSWORD} className={s.formLink}>Click Here</NavLink>
+                        </div>
+                        <div>
+                            <span className={s.formBlockText}>Don't have an account</span>
+                            <NavLink to={PATH.REGISTRATION} className={s.formLink}>Sign up</NavLink>
+                        </div>
+                    </div>
+
                 </div>
+
             </div>
         </div>
     )
