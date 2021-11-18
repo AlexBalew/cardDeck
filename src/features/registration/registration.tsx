76932987852350c28079d2a3a2a99047bbd7640a
Sil9 <sil9gosy@mail.ru>
2021-11-18 20:24:05 +0300

    add variation visibility password

--- src/features/registration/registration.tsx
+++ src/features/registration/registration.tsx
@@ -7,11 +7,15 @@ import {registryTC, setErrorAC} from "./registration-reducer";
 import {RequestStatusType} from "../../app/app-reducer";
 import SuperButton from "../../common/elements/button/SuperButton";
 import SuperInput from "../../common/elements/input/SuperInput";
+import viewPassword from "../../assets/viewPassword.png";
+import hiddenPassword from "../../assets/hiddenPassword.png";
 
 export const Registration = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirm, setConfirm] = useState('')
+    const [type, setType] = useState('password')
+    const [showPassword, setShowPassword] = useState<boolean>(true);
 
     const backError = useSelector<AppStateType, string>((state) => state.registration.backError)
     const isRegistry = useSelector<AppStateType, boolean>((state) => state.registration.isRegistry)
@@ -35,6 +39,16 @@ export const Registration = () => {
         setConfirm('')
     }
 
+    const handleShowPassword = () => {
+        if (showPassword) {
+            setType('text')
+        } else {
+            setType('password')
+        }
+
+        setShowPassword(!showPassword)
+    }
+
     if (isRegistry) return <Navigate to='/login'/>
 
     return <div className={s.mainContainer}>
@@ -46,31 +60,45 @@ export const Registration = () => {
             <div className={s.form}>
 
                 <form>
-                    <div>Email <SuperInput type={"email"}
-                                           value={email}
-                                           id={"email"}
-                                           onChange={(e) => {
-                                               dispatch(setErrorAC(''))
-                                               setEmail(e.currentTarget.value)
-                                           }}
-                                           style={{border: '1px solid black'}}/>
-                    </div>
-                    <div className={s.password}>Password <SuperInput type={"password"}
-                                              value={password}
-                                              id={"password"}
-                                              onChange={(e) => {
-                                                  setPassword(e.currentTarget.value)
-                                              }}
-                                              style={{border: '1px solid black'}}/><br/>
+                    <div>Email{<br/>}
+                        <SuperInput type={"email"}
+                                    value={email}
+                                    id={"email"}
+                                    onChange={(e) => {
+                                        dispatch(setErrorAC(''))
+                                        setEmail(e.currentTarget.value)
+                                    }}
+                                    style={{border: '1px solid black'}}/>
                     </div>
 
-                    <div>Confirm password <SuperInput type={"password"}
-                                                      value={confirm}
-                                                      id={confirm}
-                                                      onChange={(e) => {
-                                                          setConfirm(e.currentTarget.value)
-                                                      }}
-                                                      style={{border: '1px solid black'}}/><br/>
+                    <div className={s.password}>Password{<br/>}
+                        <SuperInput type={type}
+                                    value={password}
+                                    id={"password"}
+                                    onChange={(e) => {
+                                        setPassword(e.currentTarget.value)
+                                    }}
+                                    style={{border: '1px solid black'}}/>
+                        <img className={s.passwordControl}
+                             src={showPassword ? viewPassword : hiddenPassword}
+                             alt="eye"
+                             onClick={handleShowPassword}
+                        />
+                    </div>
+
+                    <div className={s.password}>Confirm password{<br/>}
+                        <SuperInput type={type}
+                                    value={confirm}
+                                    id={confirm}
+                                    onChange={(e) => {
+                                        setConfirm(e.currentTarget.value)
+                                    }}
+                                    style={{border: '1px solid black'}}/>
+                        <img className={s.passwordControl}
+                             src={showPassword ? viewPassword : hiddenPassword}
+                             alt="eye"
+                             onClick={handleShowPassword}
+                        />
                     </div>
                     <SuperButton name='sendCurrentEmail'
                                  onClick={onCancelHandle}>
@@ -82,7 +110,8 @@ export const Registration = () => {
                         Registry
                     </SuperButton>
 
-                    {password !== confirm ? <div>{'Passwords don\'t match'}</div> : ''}
+                    {password !== confirm ?
+                        <div style={{color: 'red', marginTop: '5px'}}>{'Passwords don\'t match'}</div> : ''}
                     {backError && <div style={{color: 'red', marginTop: '5px'}}>{backError}</div>}
                 </form>
             </div>
