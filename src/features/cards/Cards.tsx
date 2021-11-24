cb04b0a36b5855d852f75ba63dfed8c5c906d536
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2021-11-24 16:43:04 +0300

    m20 - created a back button on a page  and changed Cards.tsx and style for it

--- src/features/cards/Cards.tsx
+++ src/features/cards/Cards.tsx
@@ -2,63 +2,78 @@ import React, {useEffect} from 'react'
 import s from './Cards.module.css'
 import SuperButton from "../../common/elements/button/SuperButton";
 import {useDispatch, useSelector} from "react-redux";
-import {createCards, deleteCard, getCards} from "./cards-reducer";
+import {getCards} from "./cards-reducer";
 import {AppStateType} from "../../bll/store";
 import {CardType} from "../../api/cards-api";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {Card} from "./card/Card";
+import Pagination from "../../common/components/pagination/pagination";
+import {SelectPage} from "../../common/components/selectPage/SelectPage";
 
 
 export const Cards = () => {
     const dispatch = useDispatch()
 
+
     const cards = useSelector<AppStateType, Array<CardType>>(state => state.cards.cards)
     const page = useSelector<AppStateType, number>(state => state.cards.page)
+    const packName = useSelector<AppStateType, string>(state => state.cards.packName)
     const cardId = "619c20e519837f019f391b23"
     const {packId} = useParams<'packId'>()
+    const navigate = useNavigate()
 
 
     useEffect(() => {
         dispatch(getCards(packId!))
     }, [dispatch, packId, page])
 
-    const copyCards = cards.map(el => <tbody key={el._id}>
-    <Card card={el} packID={packId!}/>
-    </tbody>)
-
 
     return (
         <div className={s.container}>
-            <h1>Cards</h1>
+
+            {/* <div className={s.backLink}>
+                <NavLink className={s.link} to={'/packs-list'}>
+                    <i className={s.arrow}></i>Back{packName}
+                </NavLink>
+            </div>*/}
+
             <div className={s.cardsContainer}>
-                <div>
-                    {
-                        !cards.length
-                            ? <div>
-                                <span>There are no cards in this pack...</span>
+
+                <div className={s.backLink}>
+                    <div onClick={() => navigate('/packs-list')} className={s.arrowBack}>&larr;{packName}</div>
+                </div>
+
+                <div className={s.search}>
+                    Search
+                    <SuperButton onClick={() => dispatch(getCards(packId!))}>ADD NEW CARD</SuperButton>
+                </div>
+
+                <div className={s.cardsTable}>
+                    {!cards.length
+                        ? <div className={s.noCards}>
+                            <span>There are no cards in this pack...</span>
+                        </div>
+                        :
+                        <div className={s.cards}>
+                            <div className={s.card}>
+                                <div>Question</div>
+                                <div>Answer</div>
+                                <div>
+                                    <div><span>Last Updated</span></div>
+                                    <div><span>Grade</span></div>
+                                    <div>Actions</div>
+                                </div>
                             </div>
-                            : <table>
-                                <thead>
-                                <tr>
-                                    <th>QUESTION</th>
-                                    <th>ANSWER</th>
-                                    <th>LAST UPDATE</th>
-                                    <th>GRADE</th>
-                                    <th>ACTIONS</th>
-                                </tr>
-                                </thead>
-                                {copyCards}
-                            </table>
+                            {cards.map(el =>
+                                <div key={el._id}>
+                                    <Card card={el} packID={packId!}/>
+                                </div>)}
+                        </div>
                     }
                 </div>
-                <SuperButton onClick={() => dispatch(getCards(packId!))}>GET CARDS</SuperButton>
-                <SuperButton onClick={() => dispatch(createCards(packId!, 'What is it?', 'It is answer.'))}>CREATE
-                    CARD</SuperButton>
-                <SuperButton onClick={() => dispatch(deleteCard(cardId, packId!))}>DELETE CARD</SuperButton>
+                <div className={s.paginator}><Pagination numberOfPagesInOnePortion={6}/></div>
             </div>
-
-        </div>
-    )
+        </div>)
 }
 
 
