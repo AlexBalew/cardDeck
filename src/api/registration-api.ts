d623d33e798531414b44df2d9b05272056e63518
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2021-11-26 16:02:28 +0300

    m27 - changed api

--- src/api/registration-api.ts
+++ src/api/registration-api.ts
@@ -1,7 +1,7 @@
-import {instance_local, instance_release} from "./api";
+import {instance} from "./api";
 
 export const registApi = {
     register(email: string, password: string) {
-        return instance_local.post<{addedUser: {}, error?: string}>(`auth/register`, {email, password}, {})
+        return instance.post<{addedUser: {}, error?: string}>(`auth/register`, {email, password}, {})
     }
 }
