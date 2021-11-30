204b7229f0478d4fd4f999c6f2c4d955a01e5f02
Sil9 <sil9gosy@mail.ru>
2021-11-30 23:06:49 +0300

    ability to change name and ava

--- src/features/profile/profile.tsx
+++ src/features/profile/profile.tsx
@@ -1,14 +1,17 @@
-import React, {useEffect, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import s from "./profile.module.css"
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "../../bll/store";
 import {Navigate} from "react-router-dom";
 import {PATH} from "../../app/Routes";
-import userDefaultImg from "../../assets/profile/userImg.png"
+import userDefaultImg from "../../assets/profile/userDefaultImg.png"
 import Preloader from "../../common/components/preloader/Preloader";
 import {RequestStatusType} from "../../app/app-reducer";
+import SuperEditableSpan from "../../common/elements/editableSpan/SuperEditableSpan";
+import {changeUserData} from "./profile-reducer";
 
 export const Profile = React.memo(() => {
+    const dispatch = useDispatch()
 
     const isLoggedIn = useSelector<AppStateType, boolean>(state => state.login.isLoggedIn)
     const status = useSelector<AppStateType, RequestStatusType>(state => state.app.status)
@@ -16,19 +19,28 @@ export const Profile = React.memo(() => {
     const avatar = useSelector<AppStateType, string>(state => state.profile.avatar)
     const email = useSelector<AppStateType, string | null>(state => state.profile.email)
 
-
     const [myName, setMyName] = useState('')
-    const [myAvatar, setMyAvatar] = useState<string>('Avatar is not defined')
+    const [myAvatar, setMyAvatar] = useState<string | undefined>(undefined)
 
-    console.log('avatar: ', avatar)
     useEffect(() => {
         setMyName(name)
         if (avatar) {
             setMyAvatar(avatar)
         }
+
     }, [name, avatar])
     if (status === 'loading') {
         return <Preloader/>
+    }
+
+    const changeNameHandle = (e: ChangeEvent<HTMLInputElement>) => {
+        setMyName(e.currentTarget.value)
+    }
+    const setUserDataHandler = () => {
+        dispatch(changeUserData({name: myName, avatar: myAvatar}))
+    }
+    const changeImageHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      setMyAvatar(e.currentTarget.value)
     }
 
     if (!isLoggedIn) {
@@ -40,9 +52,11 @@ export const Profile = React.memo(() => {
             <div className={s.profileBlock}>
                 <div className={s.content}>
                     <div className={s.profileAvatar}>
-                        <img src={`${avatar !== 'Avatar is not defined' ? myAvatar : userDefaultImg}`} alt="ava"/>
+                        <img src={avatar ? avatar : userDefaultImg}  alt="ava"/>
                     </div>
-                    <div className={s.profileName}>{myName}</div>
+                    <label>Change image <input type="text" onChange={changeImageHandler} onBlur={setUserDataHandler}/></label>
+                    {/*<div className={s.profileName}>{myName}</div>*/}
+                    <SuperEditableSpan className={s.profileName} value={myName} onChange={changeNameHandle} onBlur={setUserDataHandler}/>
                     <div>{email ? email : "email not write"}</div>
                 </div>
             </div>
